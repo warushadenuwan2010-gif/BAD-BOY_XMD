70d7756ce479f702033244cf5ed50b6c93b0e4a8
warushadenuwan2010-gif <warushadenuwan2010@gmail.com>
2025-10-31 09:36:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/BAD_BOY-MD/BAD_BOY-MD/blob/main/images/BAD_BOY-MD.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 BAD_BOY-MD Is Alive Now😍*",
+BOT_OWNER: '94774756599',  // Replace with the owner's phone number
 
 
 
